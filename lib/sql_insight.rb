e4226c18f3cbd8d90d573a1fba5e273f44a28cdc
Takahiro Ebato <takahiro.ebato@gmail.com>
2024-02-15 22:00:47 +0900

    initial commit

---
+++ lib/sql_insight.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "sql_insight/version"
+require_relative "sql_insight/sql_insight"
+
+module SqlInsight
+  class Error < StandardError; end
+  # Your code goes here...
+end
